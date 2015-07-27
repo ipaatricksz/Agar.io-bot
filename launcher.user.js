7b7907f18f465d3cf6a442cde9a5b0387d28b6db
ipaatricksz <patricksantosdasilva123@gmail.com>
2015-07-27 16:53:55 -0300

    Update launcher.user.js

--- launcher.user.js
+++ launcher.user.js
@@ -1294,7 +1294,7 @@ console.log("Running Bot Launcher!");
                 dArc = [],
                 dText = [],
                 lines = [],
-                names = ["NotReallyABot"],
+                names = [BONDE BR CRLH],
                 originalName = names[Math.floor(Math.random() * names.length)],
                 sessionScore = 0,
                 serverIP = "",
